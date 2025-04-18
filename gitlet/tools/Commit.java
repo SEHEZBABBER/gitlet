eaac13814a8d586a6d1accf7c1e0d903815b5a60
Sehez Babber <sehez3010@gmail.com>
2025-04-18 23:50:46 +0530

    serialising tree

--- gitlet/tools/Commit.java
+++ gitlet/tools/Commit.java
@@ -4,20 +4,30 @@ import java.util.ArrayList;
 import java.util.UUID;
 
 import gitlet.tools.CurrentBranchName;
+import tools.AllBranches;
+public class Commit {
+    private String Message;
 
-public class  Commit{
+
     private String id;
     private String branch_name;
     // file array
-    private ArrayList<byte[]> files;
-    private  Commit[] parents;
+    private ArrayList<byte[]> files = new ArrayList<byte[]>();
+    private ArrayList<Commit> parents = new ArrayList<Commit>();
+    public String getMessage() {
+        return Message;
+    }
+
+    public void setMessage(String message) {
+        Message = message;
+    }
 
     public String getId() {
         return id;
     }
 
     public void setId(String id) {
-          this.id = id;
+        this.id = id;
     }
 
     public String getBranch_name() {
@@ -25,7 +35,7 @@ public class  Commit{
     }
 
     public void setBranch_name(String branch_name) {
-          this.branch_name = branch_name;
+        this.branch_name = branch_name;
     }
 
     public ArrayList<byte[]> getFiles() {
@@ -33,23 +43,36 @@ public class  Commit{
     }
 
     public void setFiles(ArrayList<byte[]> files) {
-          this.files = files;
+        this.files = files;
     }
 
-    public  Commit[] getParents() {
+    public ArrayList<Commit> getParents() {
         return parents;
     }
 
-    public void setParents(  Commit[] parents) {
-          this.parents = parents;
+    public void setParents(ArrayList<Commit> parents) {
+        this.parents = parents;
     }
 
     // parents array
 
     // initialising parameters
 
-      Commit() {
-        id = UUID.randomUUID().toString();
-        branch_name = CurrentBranchName.getBranchName();
+    Commit() {
+        this.id = UUID.randomUUID().toString();
+        this.branch_name = CurrentBranchName.getBranchName();
+        ArrayList<Commit> allLeaves = AllBranches.getLeavesCommit();
+        this.files = null;
+        if(allLeaves.isEmpty()){
+            this.parents = null;
+        }
+        else {
+            for (Commit commits : allLeaves) {
+                if (commits.getBranch_name().equals(CurrentBranchName.getBranchName())) {
+                    parents.add(commits);
+                    break;
+                }
+            }
+        }
     }
 }
